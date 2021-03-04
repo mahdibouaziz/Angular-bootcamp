b4f1f0f0481a8b18d0bb006f125defdd22c6dbd8
Mahdi <mahdibouaziz@hotmail.fr>
2021-03-04 19:58:50 +0100

    authentification

---
+++ src/app/authentification.service.ts
@@ -0,0 +1,14 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthentificationService {
+  link = 'http://localhost:3000/api/Users/login';
+  constructor(private http: HttpClient) {}
+  login(credentials): Observable<any> {
+    return this.http.post(this.link, credentials);
+  }
+}
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthentificationService } from '../authentification.service';
 
 @Component({
   selector: 'app-login',
@@ -7,11 +9,27 @@ import { NgForm } from '@angular/forms';
   styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
-  constructor() {}
+  constructor(
+    private authentificationService: AuthentificationService,
+    private router: Router
+  ) {}
 
   ngOnInit(): void {}
 
-  login(loginFormulaire: NgForm): void {
-    console.log(loginFormulaire);
+  login(credentials): void {
+    this.authentificationService.login(credentials).subscribe(
+      (response) => {
+        const token = response.id;
+        localStorage.setItem('token', token);
+        const link = ['cv'];
+        this.router.navigate(link);
+      },
+      (error) => {
+        console.log(error);
+      },
+      () => {
+        console.log('completed');
+      }
+    );
   }
 }
